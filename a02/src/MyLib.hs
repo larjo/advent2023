f4625adb971deb9bc1330878b90ad45b252d3193
Lars <lars.baker@uc.se>
2023-12-05 22:32:04 +0100

    Clean up

--- a02/src/MyLib.hs
+++ a02/src/MyLib.hs
@@ -10,42 +10,9 @@ import Data.Void
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import Text.Megaparsec.Debug
-import qualified Data.Text as T
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
-
-data Scheme
-  = SchemeData
-  | SchemeFile
-  | SchemeFtp
-  | SchemeHttp
-  | SchemeHttps
-  | SchemeIrc
-  | SchemeMailto
-  deriving (Eq, Show)
-
-pScheme :: Parser Scheme
-pScheme = choice
-  [ SchemeData   <$ string "data"
-  , SchemeFile   <$ string "file"
-  , SchemeFtp    <$ string "ftp"
-  , SchemeHttps  <$ string "https"
-  , SchemeHttp   <$ string "http"
-  , SchemeIrc    <$ string "irc"
-  , SchemeMailto <$ string "mailto" ]
-
-data Uri = Uri
-  { uriScheme :: Scheme
-  } deriving (Eq, Show)
-
-pUri :: Parser Uri
-pUri = do
-  r <- pScheme
-  _ <- char ':'
-  return (Uri r)
-
---------------------------
 
 data BallColor =
     Red
