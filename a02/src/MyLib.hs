305a9c604717213bbeaf9b2c64e4a161f1b3819e
Lars <lars.baker@uc.se>
2023-12-05 23:34:01 +0100

    Refactor and back to monadic style

--- a02/src/MyLib.hs
+++ a02/src/MyLib.hs
@@ -22,47 +22,54 @@ data BallColor =
   | Green
   deriving (Eq, Show)
 
-pBallColor :: Parser BallColor
-pBallColor = choice
-  [ Red   <$ string "red"
-  , Blue  <$ string "blue"
-  , Green <$ string "green" ]
-
 data Ball = Ball
   { count :: Int
   , color :: BallColor
   } deriving (Eq, Show)
 
-pBall :: Parser Ball
-pBall = Ball <$> (spaceChar *> L.decimal <* spaceChar) <*> pBallColor
-
 data Balls = Balls
   { balls :: [Ball]
   } deriving (Eq, Show)
-
-pBalls :: Parser Balls
-pBalls = Balls <$> sepBy pBall (char ',')
 
 data Game = Game
   { number :: Int
   , turn :: [Balls]
   } deriving (Eq, Show)
 
+pBallColor :: Parser BallColor
+pBallColor = choice
+  [ Red   <$ string "red"
+  , Blue  <$ string "blue"
+  , Green <$ string "green" ]
+
+pBall :: Parser Ball
+pBall = do
+  _ <- spaceChar
+  count <- L.decimal
+  _ <- spaceChar
+  color <- pBallColor
+  return (Ball count color)
+
+pBalls :: Parser Balls
+pBalls = Balls <$> sepBy pBall (char ',')
+
 pGame :: Parser Game
-pGame = Game <$> (string "Game" *> spaceChar *> L.decimal <* char ':') <*> sepBy pBalls (char ';')
+pGame = do
+  _ <- string "Game"
+  _ <- spaceChar
+  number <- L.decimal
+  _ <- char ':'
+  turn <- sepBy pBalls (char ';')
+  return (Game number turn)
 
 pGames :: Parser [Game]
-pGames = endBy pGame (char '\n')
+pGames = endBy pGame eol
 
 someFunc :: IO ()
 someFunc =
   do
     text <- readFile "assets/input.txt"
-
     let parsed = parse pGames "" (pack text)
-
     case parsed of
       Left err -> putStrLn $ errorBundlePretty err
       Right games -> print games
-
-
