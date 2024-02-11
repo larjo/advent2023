be88f53a1ef7c7174173cf4791c3ceebd2d9eba8
Lars <lars.baker@uc.se>
2024-02-11 22:14:31 +0100

    Parse input text

--- a18/Program.fs
+++ a18/Program.fs
@@ -1,3 +1,37 @@
-﻿let f a = a + 1
+﻿open System
+open System.IO
 
-printfn "Hello World from F#!"
+type Direction = 
+    | Up
+    | Down
+    | Left
+    | Right
+
+type Instruction = {
+    Direction: Direction
+    Distance: int
+}
+
+let readInput (filePath : string) = 
+    File.ReadAllLines(filePath)
+    |> Seq.map (fun (line: string) -> line.Split(' '))
+    |> Seq.map (fun line -> 
+        let direction = 
+            match line[0] with
+            | "U" -> Up
+            | "D" -> Down
+            | "L" -> Left
+            | "R" -> Right
+            | _ -> failwith "Invalid direction"
+        let distance = int line[1]
+        { Direction = direction; Distance = distance }
+    )
+
+let arr = 
+    readInput "assets/example.txt"
+    |> Seq.fold (fun (acc: int array2d) (x : Instruction) ->
+        acc) (Array2D.zeroCreate 10 10)
+
+
+Seq.iter (printfn "%A") <| readInput "assets/example.txt"
+
