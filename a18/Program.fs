0b8310eac1e65002872052cb63ae8cdfa73cbaf5
Lars <lars.baker@uc.se>
2024-02-15 22:02:39 +0100

    Part 1 (to high)

--- a18/Program.fs
+++ a18/Program.fs
@@ -27,11 +27,125 @@ let readInput (filePath : string) =
         { Direction = direction; Distance = distance }
     )
 
-let arr = 
-    readInput "assets/example.txt"
-    |> Seq.fold (fun (acc: int array2d) (x : Instruction) ->
-        acc) (Array2D.zeroCreate 10 10)
+type Field = {
+    mutable X: int
+    mutable Y: int
+    Arr: bool array2d
+}
+module Field =
+    let create width height =
+        { 
+            X = width / 2
+            Y = height / 2
+            Arr = Array2D.zeroCreate height width
+        }
+    let goTo x y (field : Field) =
+        field.X <- x
+        field.Y <- y
+    let move (direction : Direction) (field : Field) =
+        match direction with
+        | Up -> field.Y <- field.Y - 1
+        | Down -> field.Y <- field.Y + 1
+        | Left -> field.X <- field.X - 1
+        | Right -> field.X <- field.X + 1
+    
+    let digHole (field : Field) =
+        try
+            field.Arr[field.Y, field.X] <- true
+        with
+        | :? IndexOutOfRangeException ->
+            printfn $"Out of bounds {field.Y} {field.X}"
+            reraise()
 
+    let print (field : Field) =
+        for i = 0 to Array2D.length1 field.Arr - 1 do
+            for j = 0 to Array2D.length2 field.Arr - 1 do
+                printf "%s" (if field.Arr[i, j] then "#" else ".")
+            printfn ""
 
-Seq.iter (printfn "%A") <| readInput "assets/example.txt"
+let digTrench (field : Field) (instruction : Instruction) =
+    for _ in 1 .. instruction.Distance do
+        do field |> Field.digHole
+        do field |> Field.move instruction.Direction
+let boolToInt (b : bool) = if b then 1 else 0
 
+let array2DtoRows (arr : bool array2d) =
+    seq {
+        for i = 0 to Array2D.length1 arr - 1 do
+            seq {
+                for j = 0 to Array2D.length2 arr - 1 do
+                    yield arr[i, j]
+            }
+    }
+
+type State =
+    | Inside
+    | Outside
+    | Left
+    | Right
+module State =
+    let toBool (state : State) =
+        match state with
+        | Inside -> true
+        | Outside -> false
+        | Left -> true
+        | Right -> true
+
+let fillRowSeq (row : seq<bool>)  =
+    row
+    |> Seq.scan (fun state hole ->
+        match (state, hole) with
+        | (Outside, false) -> Outside
+        | (Outside, true) -> Left
+        | (Left, true) -> Left
+        | (Left, false) -> Inside
+        | (Inside, false) -> Inside
+        | (Inside, true) -> Right
+        | (Right, true) -> Right
+        | (Right, false) -> Outside
+    ) Outside
+    |> Seq.tail
+
+let fillRowsSeq (rows : bool seq seq) =
+    rows
+    |> Seq.map (fillRowSeq >> Seq.map State.toBool)
+
+let printRowSeq (row : seq<bool>) =
+    row
+    |> Seq.map (fun b -> if b then "#" else ".")
+    |> String.concat " "
+    |> printfn "%s"
+do
+    fillRowSeq [false; true; false; false; true; false; false ]
+    |> Seq.map (State.toBool >> boolToInt >> string) 
+    |> String.concat " " 
+    |> printfn "%s"
+
+let fillRow (field : Field) =
+    let mutable inside = false
+    for x = 0 to Array2D.length2 field.Arr - 1 do
+        if field.Arr[field.Y, x] then
+            if inside then
+                inside <- false
+            else
+                inside <- true
+        else
+            if inside then
+                field.Arr[field.Y, x] <- true
+let fillField (field : Field) =
+    for y = 0 to Array2D.length1 field.Arr - 1 do
+        do field.Y <- y
+        do fillRow field
+
+let countHoles (rows : bool seq seq) =
+    rows
+    |> Seq.sumBy (Seq.sumBy boolToInt)
+
+let field = Field.create 600 600
+do
+    readInput "assets/input.txt"
+    |> Seq.iter (digTrench field)
+    Field.print field
+    let filled = field.Arr |> array2DtoRows |> fillRowsSeq
+    filled |> Seq.iter printRowSeq
+    printfn "%i" (countHoles filled)
